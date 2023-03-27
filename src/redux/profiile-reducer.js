d9abbc76d0d9d0196d77851baae76d0cdf85a97c
Martyn <usengalibeka@gmail.com>
2023-03-27 17:49:04 +0600

    make a copy of the state DialogReducer

--- src/redux/profiile-reducer.js
+++ src/redux/profiile-reducer.js
@@ -19,17 +19,19 @@ const profileReducer = (state = initialState, action) => {
                 message: state.newPostText,
                 likesCount: 0
             };
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         }
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
+
+    case UPDATE_NEW_POST_TEXT: {
+            
+            return {
+                ...state,
+                newPostText: action.newText
+            };
         }
         default:
             return state;
