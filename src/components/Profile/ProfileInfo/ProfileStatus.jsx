2b4008913f4f6c9eca10991a7b173c6a3c74dfc1
Martyn <usengalibeka@gmail.com>
2023-04-20 02:28:53 +0600

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -23,11 +23,21 @@ class ProfileStatus extends React.Component {
   onStatusChange = (e) => {
     this.setState({
       status: e.currentTarget.value
-  })
-    
+    })
+
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState ({
+      status: this.props.status
+      });
+  }
+  console.log('componentDidUpdate')
+}
   
   render() {
+    console.log('render')
     return (
       <div>
         {!this.state.editMode &&
