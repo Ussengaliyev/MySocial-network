9c4437a0d1f4f6a356e11223d53a3d127dab3044
Martyn <usengalibeka@gmail.com>
2023-04-27 00:59:33 +0600

    first time use Hooks ☻

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Preloader from '../../common/Preloader';
 import s from './ProfileInfo.module.css';
-import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 
 const ProfileInfo = (props) => {
@@ -17,7 +17,7 @@ return (
       </div > */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        <ProfileStatus status = {props.status} updateStatus = {props.updateStatus}/>
+        <ProfileStatusWithHooks status = {props.status} updateStatus = {props.updateStatus}/>
       </div>
     </div >);
 }
